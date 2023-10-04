e202dbcb1ddf0b48287d1412e8ce2752396f3585
pandian <pandian@hippo.com>
2023-10-04 18:06:09 +0530

    sample

---
+++ 1.ps1
@@ -0,0 +1,6 @@
+$resourceGroup="sample"
+$location="East US"
+$networkName="s-vnet"
+$AddressPrefix="172.16.0.0/16"
+
+New-AzResourceGroup -Name $networkName  -ResourceGroupName $resourceGroup -Location $location -AddressPrefix $AddressPrefix 
