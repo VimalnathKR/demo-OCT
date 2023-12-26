f0748755d5e6024bbdd1a4ace71fa4a3ea519283
VimalnathKR <154397210+VimalnathKR@users.noreply.github.com>
2023-12-26 16:44:59 +0530

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -4,3 +4,7 @@ $networkName="s-vnet"
 $AddressPrefix="172.16.0.0/16"
 
 New-AzResourceGroup -Name $networkName  -ResourceGroupName $resourceGroup -Location $location -AddressPrefix $AddressPrefix 
+
+
+
+Adding new line
